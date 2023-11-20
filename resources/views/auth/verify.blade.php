439a88e23338788dd766722cba01d66a82cb753f
Alfian <112760996+alfian8752@users.noreply.github.com>
2023-11-20 22:05:20 +0700

    update halaman verify

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -3,10 +3,9 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
-
+        <div class="col-md-5">
+            <div class="card text-center">
+                {{-- <div class="card-header">{{ __('Verify Your Email Address') }}</div> --}}
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
@@ -14,11 +13,19 @@
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
+                    <img src="{{ asset('assets/img/Mail sent-rafiki.svg') }}" alt="" style="display: block; width: 250px; margin: auto;">
+                    <h2 class="text-center">{{ __('We have sent a verification link to your email. ') }}</h2>
+                    <br>
+                    <h5>
+                        {{ __('Please verify your email to continue. ') }}
+                    </h5>
+                    <br>
+                    <h6>
                     {{ __('If you did not receive the email') }},
+                    </h6>
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        <button type="submit" class="btn btn-primary">{{ __('click here to request another') }}</button>
                     </form>
                 </div>
             </div>
